d89ff9685d1cb926d2027eb803ca225467a3e8bc
claymorwan <claymorwan@fembois.dev>
2026-04-17 14:40:26 +0000

    2026-04-17 14:40

--- NixOS/modules/home/Theming/cursor.nix
+++ NixOS/modules/home/Theming/cursor.nix
@@ -3,10 +3,9 @@
 {
   home.pointerCursor = {
     enable = true;
-
     name = osConfig.globVars.mouseCursor.name;
+    size = osConfig.globVars.mouseCursor.size;
     package = pkgs.mouse-cursor;
-
     hyprcursor = {
       enable = true;
       size = osConfig.globVars.mouseCursor.size;
