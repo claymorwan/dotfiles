d5e13f657f3ace0335741c924211332b7810f52b
claymorwan <claymorwan@fembois.dev>
2026-05-04 19:25:22 +0000

    2026-05-04 19:25

--- NixOS/hosts/android/configuration.nix
+++ NixOS/hosts/android/configuration.nix
@@ -1,5 +1,15 @@
 {
-  imports = [
-    ../../modules/home
-  ];
+  environment.etcBackupExtension = ".bak";
+
+  nix.extraOptions = ''
+    experimental-features = nix-command flakes
+  '';
+
+  system.stateVersion = "24.05";
+
+  home-manager = {
+    config = ../../modules/home;
+    backupFileExtension = "hm-bak";
+    useGlobalPkgs = true;
+  };
 }
