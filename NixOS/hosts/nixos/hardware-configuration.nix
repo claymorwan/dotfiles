8c849dc35144d7a03107422e0e332d1026acbd34
claymorwan <claymorwan@fembois.dev>
2026-09-19 14:36:43 +0200

    2026-09-19 14:36:41

--- NixOS/hosts/nixos/hardware-configuration.nix
+++ NixOS/hosts/nixos/hardware-configuration.nix
@@ -8,13 +8,13 @@
     [ (modulesPath + "/installer/scan/not-detected.nix")
     ];
 
-  boot.initrd.availableKernelModules = [ "nvme" "xhci_pci" "ahci" "usb_storage" "usbhid" "sd_mod" ];
+  boot.initrd.availableKernelModules = [ "nvme" "xhci_pci" "ahci" "usbhid" "sd_mod" ];
   boot.initrd.kernelModules = [ ];
   boot.kernelModules = [ "kvm-amd" ];
   boot.extraModulePackages = [ ];
 
   fileSystems."/" =
-    { device = "/dev/disk/by-uuid/89392f48-672a-4e5a-91d5-393d4adf9b54";
+    { device = "/dev/mapper/luks-80763acb-1731-4299-9730-a3321e5dae37";
       fsType = "ext4";
     };
 
@@ -31,16 +31,16 @@
       fsType = "btrfs";
     };
 
-  swapDevices =
-    [ { device = "/dev/disk/by-uuid/0e7cbe6b-7b68-4212-b26f-1788d0bda75b"; }
-    ];
+  fileSystems."/mnt/ssd" =
+    { device = "/dev/mapper/luks-9a46aef1-38c3-41d5-b9bd-db8c17e659d1";
+      fsType = "btrfs";
+    };
 
-  # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
-  # (the default) this is the recommended approach. When using systemd-networkd it's
-  # still possible to use this option, but it's recommended to use it in conjunction
-  # with explicit per-interface declarations with `networking.interfaces.<interface>.useDHCP`.
-  networking.useDHCP = lib.mkDefault true;
-  # networking.interfaces.enp9s0.useDHCP = lib.mkDefault true;
+  boot.initrd.luks.devices."luks-9a46aef1-38c3-41d5-b9bd-db8c17e659d1".device = "/dev/disk/by-uuid/9a46aef1-38c3-41d5-b9bd-db8c17e659d1";
+
+  swapDevices =
+    [ { device = "/dev/mapper/luks-f39cc2fd-88f1-4b75-994c-49f9175d4506"; }
+    ];
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
   hardware.cpu.amd.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
